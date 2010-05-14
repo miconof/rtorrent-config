462ffb030987b123e1eebdf406228d1291430564
Devin <devinwadsworth@gmail.com>
2010-05-14 11:49:57 -0500

    Changed install.sh header and updated script to use checkinstall instead of make install.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 ########################################################
-# rTorrent WebUI Installer 2009 November 13 Version 4.0 #
+# rTorrent WebUI Installer 2010 May 14 Version 5.0 EXPERIMENTAL #
 ########################################################
 # Author: daymun (http://github.com/daymun)
 # Coauthor: JMV290 (http://github.com/JMV290)
@@ -55,7 +55,7 @@ function installRtorrentApt {
 
 function installRtorrentSvn {
 	# Install dependencies
-	sudo apt-get -y install autoconf automake autotools-dev binutils build-essential bzip2 ca-certificates comerr-dev cpp cpp-4.1 dpkg-dev file g++ g++-4.1 gawk gcc gcc-4.1 libapr1 libaprutil1 libc6-dev libexpat1 libidn11 libidn11-dev libkadm55 libkrb5-dev libmagic1 libncurses5-dev libneon26 libpcre3 libpq5 libsigc++-2.0-dev libsqlite0 libsqlite3-0 libssl-dev libssp0-dev libstdc++6-4.1-dev libsvn1 libtool libxml2 linux-libc-dev lynx m4 make mime-support ntp ntpdate openssl patch pkg-config ucf zlib1g-dev libcurl4-openssl-dev || fail "Failed to install dependencies."
+	sudo apt-get -y install autoconf automake autotools-dev binutils build-essential bzip2 ca-certificates checkinstall comerr-dev cpp cpp-4.1 dpkg-dev file g++ g++-4.1 gawk gcc gcc-4.1 libapr1 libaprutil1 libc6-dev libexpat1 libidn11 libidn11-dev libkadm55 libkrb5-dev libmagic1 libncurses5-dev libneon26 libpcre3 libpq5 libsigc++-2.0-dev libsqlite0 libsqlite3-0 libssl-dev libssp0-dev libstdc++6-4.1-dev libsvn1 libtool libxml2 linux-libc-dev lynx m4 make mime-support ntp ntpdate openssl patch pkg-config ucf zlib1g-dev libcurl4-openssl-dev || fail "Failed to install dependencies."
 
 	# Download and install xmlrpc
 	cd
@@ -63,7 +63,7 @@ function installRtorrentSvn {
 	cd xmlrpc-c
 	sudo ./configure --disable-cplusplus || fail "Failed to configure xmlrpc-c."
 	sudo make || fail "Failed to make xmlrpc-c."
-	sudo make install || fail "Failed to install xmlrpc-c."
+	sudo checkinstall -y || fail "Failed to install xmlrpc-c."
 
 	# Download and install rTorrent
 	cd
@@ -75,12 +75,12 @@ function installRtorrentSvn {
 	sudo ./autogen.sh
 	sudo ./configure || fail "Failed to configure libtorrent."
 	sudo make || fail "Failed to make libtorrent."
-	sudo make install || fail "Failed to install libtorrent."
+	sudo checkinstall -y || fail "Failed to install libtorrent."
 	cd ../rtorrent
 	sudo ./autogen.sh
 	sudo ./configure --with-xmlrpc-c || fail "Failed to configure rtorrent."
 	sudo make || fail "Failed to make rtorrent."
-	sudo make install || fail "Failed to install rtorrent."
+	sudo checkinstall -y || fail "Failed to install rtorrent."
 }
 
 function configureRtorrent {
